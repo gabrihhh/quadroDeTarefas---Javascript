2c2b87d0ab7cfcf771889e88d574afc927ad3d48
Gabriel Patara <gabriel.patara@gmail.com>
2024-04-22 11:21:19 -0300

    agora funciona kkk

--- js/app.js
+++ js/app.js
@@ -1,33 +1,28 @@
 const entrada = document.getElementById('entrada');
 const place = document.getElementById('place');
+
+
 let atividades =[]
-let n = 0
+let n = 1
 
 function Atualizar(atividades){
     place.innerHTML = ''
-    let n = 0
     atividades.forEach(e => {
-        place.innerHTML += '<div id="'+n+'">'+e.faz+'</div>';
-        n++
-    })
-    
-}
+        place.innerHTML += '<div id="'+e.id+'">'+e.faz+'</div>';
+    });
+};
 
 window.addEventListener('keydown',(e)=>{
-    if(e.keyCode == 13 && entrada.value !== ''){
-    let id = n
+    if(e.code == "Enter" && entrada.value !== ""){
     atividades.push({id:n,faz:entrada.value})
-    Atualizar(atividades)
-    n += 1
-    
-    }
-    
+    Atualizar(atividades);
+    n++;
+    };
 })
 
 window.addEventListener('click',(e)=>{
-
-    let array = atividades.splice(e.target.id,e.target.id)
-    Atualizar(atividades)
-    
+    const filtragem = atividades.filter((element)=> element.id !== parseInt(e.target.id));
+    atividades = filtragem
+    Atualizar(atividades);
 })
 
